1d3087cff257bc7407b689d9785993794ff57566
Stanisław Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-05-08 20:04:38 +0200

    Add pages links

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,31 @@ const app = {
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
-    thisApp.activatePage(thisApp.pages[0].id);
+
+    const idFromHash = window.location.hash.replace('#/','');
+    let  pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages){
+      if (page.id === idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href */
+        const id = clickedElement.getAttribute('href').replace('#','');
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = `#/${id}`;
+      });
+    }
   },
 
   activatePage: function(pageId){
