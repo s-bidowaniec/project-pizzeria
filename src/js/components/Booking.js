966be712ceb927434250068835fa9d693a24851d
Stanisław Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-05-14 15:46:23 +0200

    Add API get method for Booking class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import {select} from '../settings.js';
+import {select, settings} from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -10,6 +11,58 @@ class Booking {
     thisBooking.render(domContainer);
     thisBooking.getDomElements();
     thisBooking.initWidgets();
+    thisBooking.getData();
+  }
+
+  getData() {
+    const thisBooking = this;
+
+    const startDateParam = `${settings.db.dateStartParamKey}=${utils.dateToStr(thisBooking.widgets.date.minDate)}`;
+    const endDateParam = `${settings.db.dateEndParamKey}=${utils.dateToStr(thisBooking.widgets.date.maxDate)}`;
+
+    const params = {
+      bookings: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+    //console.log('getData params', params);
+    const urls = {
+      bookings: `${settings.db.url}/${settings.db.booking}?${params.bookings.join('&')}`,
+      eventsCurrent: `${settings.db.url}/${settings.db.event}?${params.eventsCurrent.join('&')}`,
+      eventsRepeat: `${settings.db.url}/${settings.db.event}?${params.eventsRepeat.join('&')}`,
+    };
+    //console.log(urls);
+    Promise.all([
+      fetch(urls.bookings),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function (allResponses) {
+        const bookingResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function ([bookings, eventsCurrent, eventsRepeat]) {
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
+
   }
 
   render(domElement) {
@@ -39,11 +92,11 @@ class Booking {
     thisBooking.widgets = {};
     thisBooking.widgets.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.dom.peopleAmount.addEventListener('updated', () => {
-      console.log(thisBooking.widgets.peopleAmount.value);
+      //console.log(thisBooking.widgets.peopleAmount.value);
     });
     thisBooking.widgets.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.dom.hoursAmount.addEventListener('updated', () => {
-      console.log(thisBooking.widgets.hoursAmount.value);
+      //console.log(thisBooking.widgets.hoursAmount.value);
     });
     thisBooking.widgets.date = new DatePicker(thisBooking.dom.date);
     thisBooking.widgets.time = new HourPicker(thisBooking.dom.time);
